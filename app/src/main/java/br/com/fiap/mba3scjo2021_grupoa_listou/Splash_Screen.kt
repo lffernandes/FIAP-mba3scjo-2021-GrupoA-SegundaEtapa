98d30c3b652b4f98161847114d456d285ec7e4da
Luiz Felipe <luiz.fernandesgti@gmail.com>
2021-10-23 15:50:22 -0300

    Create login and home activity

---
+++ app/src/main/java/br/com/fiap/mba3scjo2021_grupoa_listou/Splash_Screen.kt
@@ -0,0 +1,11 @@
+package br.com.fiap.mba3scjo2021_grupoa_listou
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
