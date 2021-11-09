2202e99bbc4afe4b85b168592d53c629662ce806
ViniciusItoi <30914356+ViniciusItoi@users.noreply.github.com>
2021-11-09 02:07:05 -0300

    update  activity_new_edit_lista

--- app/src/main/java/br/com/fiap/mba3scjo2021_grupoa_listou/NewEditListaActivity.kt
+++ app/src/main/java/br/com/fiap/mba3scjo2021_grupoa_listou/NewEditListaActivity.kt
@@ -1,6 +1,10 @@
 package br.com.fiap.mba3scjo2021_grupoa_listou
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
@@ -8,15 +12,23 @@ import br.com.fiap.mba3scjo2021_grupoa_listou.Services.RetrofitService
 import br.com.fiap.mba3scjo2021_grupoa_listou.models.ListaCompra
 import retrofit2.Response
 
-class NewEditListaActivity : AppCompatActivity() {
+class NewEditListaActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener{
 
     private lateinit var retService: RetrofitService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_edit_lista)
-       /* retService = ApiFactory.buildService(RetrofitService::class.java)
+
+        /* retService = ApiFactory.buildService(RetrofitService::class.java)
         uploadLista()*/
+
+        val spinner: Spinner = findViewById(R.id.periodo_spinner)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(this, R.array.periodo_array, android.R.layout.simple_spinner_item).also {
+            adapter -> adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
 
     }
 
@@ -50,5 +62,16 @@ class NewEditListaActivity : AppCompatActivity() {
                     " " + "Recorrencia: ${receivedListaItem?.recorrencia}" + "\n" +
                     " " + "Orcamento: ${receivedListaItem?.orcamento}" + "\n\n\n"
         })*/
+    }
+    //funções para trabalhar com o dropdown(spinner)
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        val spinner: Spinner = findViewById(R.id.periodo_spinner)
+        spinner.onItemSelectedListener = this
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback
     }
 }
